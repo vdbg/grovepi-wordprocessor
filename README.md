db6b51e0ee06a0d49ce53f46098a79e71787e44c
vdbg <vdb_g@hotmail.com>
2023-03-11 16:34:20 -0800

    v1.0
    working prototype

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# grovepi-wordprocessor
+# Grovepi Wordprocessor
+
+## Background
+
+I wanted a stand-alone wordprocessor, similar to [this one](https://www.amazon.com/Alphasmart-NEO-AA-0410-10971-AQ-Neo-Handheld/dp/B007BHWRII), meaning a device that can only be used
+to type text, and absolutely nothing else (no notifications, no browser, no distractions) and had an old [Raspberry Pi Model B](https://en.wikipedia.org/wiki/Raspberry_Pi) and [GrovePI](https://www.dexterindustries.com/grovepi/) kit, both taking dust for over a decade.
+
+Below is a description of the steps taken to make this project happen.
+
+## Hardware used
+
+* A [Raspberry Pi](https://www.raspberrypi.com/). Tested on the 2012 model B, so should work on any regular Raspberry Pi (meaning Pico and Zero models excluded). 
+* A [Grove Pi](https://www.dexterindustries.com/grovepi/) kit. Especially the board and the LCD. It looks like it's discontinued but maybe it can be found on eBay. An alternative could be [Adafruit](https://www.adafruit.com/product/1115) but I don't have one.
+* A keyboard. 
+
+## Installation
+
+### Required
+
+* Attach the GrovePi board on the Raspberry Pi and the LCD screen in port I2C-2 on the board
+* Install **buster** on the Raspberry. Not bullseye or later.
+  * Bullseye or later will **[not work](https://forum.dexterindustries.com/t/grove-pi-doesnt-work-do-not-use-raspbian-bullseye/8664)** with the GrovePi kit.
+  * Buster can be downloaded from [here](https://www.raspberrypi.com/software/operating-systems/#raspberry-pi-os-legacy).
+  * The lite version (no desktop) is recommended since the whole point of the wordprocessor is to only display on an LCD screen.
+* Run `sudo raspi-config` to set the pi user to Console AutoLogin using (under System Options, Boot / Auto login)
+* Install [GrovePi](https://github.com/DexterInd/GrovePi). The below incantation (that bypasses the UI components) is recommended:
+  `curl -kL dexterindustries.com/update_grovepi | bash -s -- --bypass-gui-installation --system-wide`
+
+### Optional
+
+* Run `sudo raspi-config` to:
+  * setup Wifi, since the idea is to have something portable. Note the Raspberry Pi Model B doesn't have Wifi natively. Need a USB adapter.
+  * enable and test ssh since we'll be messing up with tty1.
+* Since the goal is to have the Raspberry Pi headless but also produce some files, you want these to be either accessible or be copied to another machine.
+  For example, install samba server to have the files accessible from Windows machines or samba client to have the files copied to Windows machines. I went with `sudo apt-get install smbclient`.
+* Setup a Bluetooth keyboard, since the idea is to have something portable. 
+  * Get a BT USB adapter (if Raspberry Pi older than model 2)
+  * `sudo bluetoothctl`, then scan on, pair, etc.
+  * You may need to trust the keyboard after to make sure it auto-connects. `sudo bluetoothctl trust <mac-address>`
+* Set up auto-updates if you want a low-maintenance device: `sudo apt install unattended-upgrades`
+* Update the firmware of the GrovePi board: 
+  `cd ~/Dexter/GrovePi/Firmware &&  sudo bash firmware_update.sh`
+
+## Script setup
+
+* Download this repo locally, e.g. `git clone https://github.com/vdbg/grovepi-wordprocessor.git` and cd inside the directory
+* Run `pip3 install -r requirements.txt`
+* Copy `template.config.toml` to `config.toml`
+* Edit `config.toml` accordingly, in particular configure where the processor should save files
+  If saving to a file share, configure it in `/etc/fstab` to automount, for example
+  `//file/share /mnt/share cifs username=user,password=passwd,auto,user,dir_mode=0777,file_mode=0666,nounix 0 0`
+* Test the script; change paths accordingly if needed:
+  `/usr/bin/python3 $HOME/grovepi-wordprocessor/word.py`
+* Edit the `~/.profile` file and add the following lines at the end (with path fixed if appropriate):
+```
+if [[ "$(tty)" == "/dev/tty1" ]]
+then
+  /usr/bin/python3 $HOME/grovepi-wordprocessor/word.py ; exit
+fi
+```
+* If logged on the console (physical monitor), press 'Ctrl-D'. This should automatically start the word processor. Now that the default tty1 console is "taken" by the word processor, you have to use CTRL+ALT+Function_Key (F1-F7) to use the other consoles.
+* If logged through ssh instead, then `ps auwx  | grep bash` and kill the bash process on tty1. The wordprocessor should automatically start.
+* Now, whenever the keyboard physically attached to the pi is used (possibly through bluetooth), the LCD screen should show what's typed.
+
---
+++ word.py
@@ -0,0 +1,146 @@
+
+#!/usr/bin/python3
+
+from datetime import datetime
+# See https://github.com/DexterInd/GrovePi/blob/master/Software/Python/grove_rgb_lcd/grove_rgb_lcd.py
+from grove_rgb_lcd import *
+from pathlib import Path
+import logging
+import os
+import platform
+import sys
+import termios
+import time
+# see comments in requirements.txt
+import tomli as tomllib
+import tty
+
+logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO)
+
+
+def get_config():
+    CONFIG_FILE = "config.toml"
+    try:
+        with open(Path(__file__).with_name(CONFIG_FILE), "rb") as config_file:
+            config = tomllib.load(config_file)
+
+            if not config:
+                raise ValueError(f"Invalid {CONFIG_FILE}. See template.{CONFIG_FILE}.")
+
+            for name in {"lcd", "files", "main"}:
+                if name not in config:
+                    raise ValueError(f"Invalid {CONFIG_FILE}: missing section {name}.")
+
+            return config
+    except FileNotFoundError as e:
+        logging.error(f"Missing {e.filename}.")
+        exit(2)
+
+
+class Processor:
+    def __init__(self, config: dict) -> None:
+        self.hello_line: str = config["lcd"]["hello_line"]
+        self.line_len: int = config["lcd"]["line_len"]
+        self.delay_msg_seconds: int = config["lcd"]["delay_msg_seconds"]
+        self.save_dir: str = config["files"]["save_dir"]
+        self.stdin_fd = sys.stdin.fileno()
+        self.old_tty_settings = termios.tcgetattr(self.stdin_fd)
+
+    def set_text_with_refresh(self, msg: str) -> None:
+        logging.debug(f"Sending text with refresh: {msg}")
+        setText(msg)
+
+    def set_text_no_refresh(self, msg: str) -> None:
+        logging.debug(f"Sending text without refresh: {msg}")
+        setText_norefresh(msg)
+
+    def set_default_color(self) -> None:
+        setRGB(0, 128, 64)  # TODO: configure
+
+    def set_error_color(self) -> None:
+        setRGB(255, 0, 0)  # TODO: configure
+
+    def set_no_color(self) -> None:
+        setRGB(0, 0, 0)
+
+    def message_and_pause(self, message: str) -> None:
+        self.set_text_with_refresh(message)
+        time.sleep(self.delay_msg_seconds)
+        self.set_default_color()
+
+    def printException(self, prefix: str, e: Exception) -> None:
+        logging.exception(str, e)
+        self.set_error_color()
+        self.message_and_pause(f"{prefix}\n{str(e)}")
+
+    def saveToFile(self, line: str) -> None:
+        try:
+            filename = datetime.today().strftime('%Y-%m-%d')
+            file_path = os.path.join(self.save_dir, filename+".txt")
+            file = open(file_path, "a")  # append mode
+            file.write(line)
+            file.write("\n")
+            file.close()
+        except Exception as e:
+            self.printException("SAVE FAILED", e)
+
+    def main(self) -> None:
+        try:
+            # this sets tty in raw mode, allowing for directly handling keyboard input w/o buffering
+            tty.setcbreak(self.stdin_fd)
+
+            previous_line: str = self.hello_line
+            current_line: str = ""
+
+            self.set_default_color()
+            self.set_text_with_refresh(previous_line)
+
+            while True:
+                c = ord(sys.stdin.read(1))
+                c = chr(c)
+                if c == '\n':
+                    previous_line = current_line[:self.line_len]  # only print the first Line-len chars
+                    self.saveToFile(current_line)
+                    current_line = ""
+                    self.set_text_with_refresh(previous_line)
+                    continue
+                current_line = current_line + c
+                display_line: str = current_line  # we may not be able to display the entire current line
+                if len(display_line) > self.line_len:
+                    display_line = display_line[-self.line_len:]  # grad the last line_len chars (make it scroll right)
+                    self.set_text_with_refresh(previous_line + "\n" + display_line)
+                else:
+                    self.set_text_no_refresh(previous_line + "\n" + display_line)
+
+        except KeyboardInterrupt:
+            print("Exiting due to Ctrl-C ...")
+        except Exception as e:
+            self.printException("ERROR", e)
+            logging.exception(e)
+
+        finally:
+            # this restores tty in regular mode
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_tty_settings)
+            self.set_default_color()
+            self.message_and_pause("Exiting")
+            self.set_no_color()
+
+
+SUPPORTED_PYTHON_MAJOR = 3
+SUPPORTED_PYTHON_MINOR = 7
+
+if sys.version_info < (SUPPORTED_PYTHON_MAJOR, SUPPORTED_PYTHON_MINOR):
+    raise Exception(f"Python version {SUPPORTED_PYTHON_MAJOR}.{SUPPORTED_PYTHON_MINOR} or later required. Current version: {platform.python_version()}.")
+
+
+try:
+    config = get_config()
+
+    logging.getLogger().setLevel(logging.getLevelName(config["main"]["log_verbosity"]))
+    logging.info("Writing typed text to LCD screen ...")
+
+    processor = Processor(config)
+    processor.main()
+
+except Exception as e:
+    logging.exception(e)
